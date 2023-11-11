b6f28550ada2e94b4cf04687487bb0795a18879d
Bruna <125412788+BDassoler@users.noreply.github.com>
2023-11-11 20:02:59 -0300

    Alterado dockerfile

--- src/test/java/br/com/senac/api/AulaPaginacaoApiApplicationTests.java
+++ src/test/java/br/com/senac/api/AulaPaginacaoApiApplicationTests.java
@@ -1,13 +1,7 @@
 package br.com.senac.api;
 
-import br.com.senac.api.entity.Pedido;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class AulaPaginacaoApiApplicationTests {
