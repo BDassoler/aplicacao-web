0c5672e159272d07a8f9d5e0f2f2347fb8f0a17d
Bruna <125412788+BDassoler@users.noreply.github.com>
2023-11-11 18:57:34 -0300

    Retirado task testes

--- src/test/java/br/com/senac/api/AulaPaginacaoApiApplicationTests.java
+++ src/test/java/br/com/senac/api/AulaPaginacaoApiApplicationTests.java
@@ -1,7 +1,13 @@
 package br.com.senac.api;
 
+import br.com.senac.api.entity.Pedido;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class AulaPaginacaoApiApplicationTests {
@@ -9,5 +15,4 @@ class AulaPaginacaoApiApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
